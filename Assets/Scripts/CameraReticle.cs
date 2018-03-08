6410a1de40eb10df3c9d2c773f3255e5cbda899b
Alberto Scicali <alberto.scicali@gmail.com>
2018-03-08 01:54:28 -0500

    Reupload commit

---
+++ Assets/Scripts/CameraReticle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraReticle : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _reticlePrefab;
+    private GameObject _reticleGO;
+    private Camera _mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        _mainCamera = Camera.main;
+        _reticleGO = Instantiate(_reticlePrefab, Vector3.zero, Quaternion.identity) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit rayHit;
+
+        if (Physics.Raycast(_mainCamera.transform.position, _mainCamera.transform.forward, out rayHit))
+        {
+            _reticleGO.transform.position = rayHit.point;
+        }
+        else {
+            _reticleGO.transform.position = _mainCamera.transform.position + _mainCamera.transform.forward * 5f;
+        }
+	}
+}
--- Assets/Scripts/NumberFactory.cs
+++ Assets/Scripts/NumberFactory.cs
@@ -9,10 +9,12 @@ public class NumberFactory : MonoBehaviour {
     private Camera _mainCamera;
     [SerializeField]
     private GameObject _textPrefab;
-
+    private bool _doneSpawningNums = false;
     private List<GameObject> _spawnedNumberObjects = new List<GameObject>();
 
     public void SpawnNumber(string number) {
+        // If done spawning numbers, then return out of method
+        if (_doneSpawningNums) return;
 
         var go = Instantiate(_textPrefab, GetSpawnLocation(), Quaternion.identity);
         go.GetComponent<TMP_Text>().text = number;
@@ -25,6 +27,7 @@ public class NumberFactory : MonoBehaviour {
             Destroy(go);
         }
         _spawnedNumberObjects = new List<GameObject>();
+        _doneSpawningNums = false;
     }
 
     public void Undo() {
@@ -32,6 +35,15 @@ public class NumberFactory : MonoBehaviour {
 
         Destroy(_spawnedNumberObjects[_spawnedNumberObjects.Count - 1]);
         _spawnedNumberObjects.RemoveAt(_spawnedNumberObjects.Count - 1);
+    }
+
+    public void Done()
+    {
+        _doneSpawningNums = true;
+    }
+
+    public void Continue() {
+        _doneSpawningNums = false;
     }
 
     private Vector3 GetSpawnLocation() {
